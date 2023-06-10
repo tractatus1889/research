f31f9ebf51b18ffb4bbd07b084394d744addc42d
KL <tractatus1889@gmail.com>
2023-06-10 16:40:44 -0700

    Run chat for __main__.

--- chat.py
+++ chat.py
@@ -5,13 +5,14 @@ from datetime import datetime
 
 
 def chat_interface():
-    print("Welcome to the Chat Interface!")
-    while True:
-        prompt = input("Your input ('exit' to exit): ")
-        if prompt.lower() == 'exit':
-            break
-        content_json = api.get_response_continue(prompt)
-        print("Response:", content_json["content"])
+  print("Welcome to the Chat Interface!")
+  while True:
+    prompt = input("Your input ('exit' to exit): ")
+    if prompt.lower() == 'exit':
+      break
+    content_json = api.get_response_continue(prompt)
+    print("Response:", content_json["content"])
 
 
-chat_interface()
+if __name__ == '__main__':
+  chat_interface()
